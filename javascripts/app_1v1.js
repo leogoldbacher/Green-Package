a3025119d3e1d084c5810b4a8ccf1bf7dc99ea03
xSabba <devall.s03@htlwienwest.at>
2021-05-04 12:56:57 +0200

    neue Dateien alg.js und alg_data.js um den Algorythmus zu testen

--- javascripts/app_1v1.js
+++ javascripts/app_1v1.js
@@ -7,6 +7,7 @@ const vm = new Vue({
     cities: data.cities,
     selectedCities: [],
     lines: [],
+    genRoutes: []
   },
   computed: {
     selectedRoute() {
@@ -96,91 +97,3 @@ const vm = new Vue({
   created() {},
 });
 
-class Walker {
-  constructor(startId, routes, emission, duration, visited, end) {
-    this.startId = startId;
-    this.routes = routes;
-    this.emission = emission;
-    this.duration = duration;
-    this.visited = visited;
-    this.end = end;
-  }
-
-  useRoute(comingFrom, goingTo, route) {
-    this.visited.push(comingFrom);
-    this.routes.push(route);
-    this.emission += route.emission;
-    this.duration += route.duration;
-    this.end = goingTo;
-  }
-
-  createDeepCopy() {
-    return new Walker(
-      this.startId,
-      [...this.routes],
-      this.emission,
-      this.duration,
-      [...this.visited],
-      this.end
-    );
-  }
-}
-
-function generateRoutes() {
-  let waitingWalkers = [];
-  let genRoutes = [];
-  for (let currentCity of data.cities) {
-    let curCityId = currentCity.id;
-    let routesFrom = data.routes.filter((e) => e.c.includes(curCityId));
-    for (let r of routesFrom) {
-      const toCityId = r.c[0] == curCityId ? r.c[1] : r.c[0];
-      if (
-        !genRoutes.find(
-          (e) => e.startCityId == curCityId && e.endCityId == toCityId
-        )
-      ) {
-        genRoutes.push({
-          startCityId: curCityId,
-          endCityId: toCityId,
-          duration: r.duration,
-          emission: r.emission,
-          routes: [r],
-        });
-        let walker = new Walker(curCityId, [], 0, 0, [], undefined);
-        walker.useRoute(curCityId, toCityId, r);
-        waitingWalkers.push(walker);
-      } else {
-        console.log("skip route");
-      }
-    }
-  }
-
-  while (waitingWalkers.length > 0) {
-    console.log(" waitingWalkers: " + waitingWalkers.length);
-    console.log("genRoutes: " + genRoutes.length);
-    let curWalker = waitingWalkers.shift();
-    let routesFrom = data.routes.filter((e) => e.c.includes(curWalker.end));
-    let curCityId = curWalker.end;
-    for (let r of routesFrom) {
-      const toCityId = r.c[0] == curCityId ? r.c[1] : r.c[0];
-      if (!curWalker.visited.includes(toCityId)) {
-        let newWalker = curWalker.createDeepCopy();
-        newWalker.useRoute(curCityId, toCityId, r);
-        genRoutes.push({
-          startCityId: curWalker.startCityId,
-          endCityId: toCityId,
-          duration: newWalker.duration,
-          emission: newWalker.emission,
-          routes: [...newWalker.routes],
-        });
-        if (genRoutes.length < 18000) {
-          waitingWalkers.push(newWalker);
-        }
-      } else {
-        console.log("city visited stop walker");
-      }
-    }
-  }
-}
-
-generateRoutes();
